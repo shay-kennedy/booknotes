9bdd9647a47e36c944a312c981c4b89c902bb873
shay-kennedy <kennedy.rshay@gmail.com>
2016-12-03 02:23:54 -0700

    Added modal form to component to capture user inputs

--- client/js/components/booknotes-list.js
+++ client/js/components/booknotes-list.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import actions from '../redux/actions';
 import { connect } from 'react-redux';
 import BooknotesDetail from './booknotes-detail';
@@ -7,22 +9,35 @@ import BooknotesDetail from './booknotes-detail';
 class BooknotesList extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+      modal: false
+    };
+    this.toggle = this.toggle.bind(this);
 		this.addBooknote = this.addBooknote.bind(this);
 	}
 
+	toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
 
 	addBooknote(e) {
 		e.preventDefault();
-		var title = prompt('Note title:');
-		var website = prompt('Note url:');
+		var title = ReactDOM.findDOMNode(this.refs.title).value.trim();
+		console.log('TITLE', title);
+		var website = ReactDOM.findDOMNode(this.refs.url).value.trim();
+		console.log('WEBSITE', website);
 		var pattern = /^((http|https|ftp):\/\/)/;
 		if(!pattern.test(website)) {
     	website = "http://" + website;
 		}
-		var note = prompt('Note note:');
+		var note = ReactDOM.findDOMNode(this.refs.note).value.trim();
+		console.log('NOTE', note);
 		if (title == null) {
 			return;
 		};
+		console.log("PASSING", title, website, note);
 		this.props.dispatch(actions.addBooknote(title, website, note, this.props.activeCategory));
 	}
 
@@ -38,17 +53,60 @@ class BooknotesList extends React.Component {
 			return (
 				<div>
 					<p>Add a Booknote to this category.</p>
-					<input type="button" value="Add Note" onClick={this.addBooknote} />
+					<Button color="danger" onClick={this.toggle}>Add Booknote</Button>
+					<Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+	          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+	          <ModalBody>
+	            <Form>
+	            	<FormGroup>
+	          		 	<Label for="title">Booknote Title:</Label>
+	        				<Input type="text" name="title" id="title" ref="title" placeholder="enter title here" />
+	            	</FormGroup>
+	            	<FormGroup>
+	            		<Label for="url">Booknote Title:</Label>
+	        				<Input type="url" name="url" id="url" ref="url" placeholder="enter url/website here" />
+	            	</FormGroup>
+	            	<FormGroup>
+	            		<Label for="note">Note:</Label>
+	          			<Input type="textarea" name="note" id="note" ref="note" placeholder="enter your note here to remind you about this booknote" />
+	            	</FormGroup>
+	            	<Button color="primary" onClick={(e) => { this.toggle(); this.addBooknote(e); }}>Submit</Button>{' '}
+	            	<Button color="secondary" onClick={this.toggle}>Cancel</Button>
+	            </Form>
+	          </ModalBody>
+	        </Modal>
 				</div>
 			)
 		};
+		console.log("HERE", this.props.category.items);
 		var booknoteList = this.props.category.items.map((item) => {
 			return (<BooknotesDetail key={item.booknote_id} item={item} activeCategory={this.props.activeCategory} />)
 		});
 		return (
 			<div id="booknote-list">
-				<input type="button" value="Add Note" onClick={this.addBooknote} />
-				{booknoteList}
+				<Button color="danger" onClick={this.toggle}>Add Booknote</Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+          <ModalBody>
+            <Form>
+            	<FormGroup>
+          		 	<Label for="title">Booknote Title:</Label>
+        				<Input type="text" name="title" id="title" ref="title" placeholder="enter title here" />
+            	</FormGroup>
+            	<FormGroup>
+            		<Label for="url">Booknote Title:</Label>
+        				<Input type="url" name="url" id="url" ref="url" placeholder="enter url/website here" />
+            	</FormGroup>
+            	<FormGroup>
+            		<Label for="note">Note:</Label>
+          			<Input type="textarea" name="note" id="note" ref="note" placeholder="enter your note here to remind you about this booknote" />
+            	</FormGroup>
+            	<Button color="primary" onClick={(e) => { this.toggle(); this.addBooknote(e); }}>Submit</Button>{' '}
+            	<Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            </Form>
+          </ModalBody>
+        </Modal>
+				{booknoteList || <div>Hi</div>}
 			</div>
 		)
 	}
